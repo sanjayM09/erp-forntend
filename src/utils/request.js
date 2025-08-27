d591b894eb8f24083a654182332048988431e198
sanjayM09 <sanjay965998@gmail.com>
2025-08-27 20:15:56 +0530

    dugg

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,12 +4,12 @@ const baseURLs = {
 
   // development: 'http://192.168.29.106:8060/', // API server url (Development)
   development: 'http://192.168.29.66:8070/', // API server url (Development)
- production: 'https://erp.dev.api.ideauxbill.in/', // API server url (Production)
+ production: 'https://erp-5vv2.onrender.com/', // API server url (Production)
   staging: 'https://erp.dev.ideauxbill.in/',  // API server url (Staging)
 
 };
-const environment = process.env.NODE_ENV || 'development';
-// const environment = 'production';
+// const environment = process.env.NODE_ENV || 'development';
+const environment = 'production';
 
 const request = axios.create({
   baseURL: baseURLs[environment],     // Sanjay
